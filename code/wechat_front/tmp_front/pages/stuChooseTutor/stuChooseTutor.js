2cf6527f8cdee9458ec7e1385802000e01557d82
S1mpleSonny <1725177475@qq.com>
2020-07-10 10:02:07 +0800

    feat:complete the stuChooseTutorPage

--- code/wechat_front/tmp_front/pages/stuChooseTutor/stuChooseTutor.js
+++ code/wechat_front/tmp_front/pages/stuChooseTutor/stuChooseTutor.js
@@ -24,7 +24,19 @@ Page({
         defaultIndex: 2,
       },
     ],
-    tutors: []
+    tutors: ['臧斌宇','陈昊鹏','JBoss'],
+    profession: "",
+    tutor: ""
+  },
+  // 绑定确认提交按钮
+  onConfirm(){
+    //向后端发消息：
+
+
+    //回到上一个页面
+
+
+
   },
   bindOpenDept(){
     this.setData({deptshow: true})
@@ -35,22 +47,23 @@ Page({
   onDeptConfirm(event){
     const { picker, value, index } = event.detail;
     var dept = value[0];
-    var perfession = value[1];
-    console.log(perfession);
-    this.setData({deptshow: false});
+    var profession = value[1];
+    console.log(profession);
+    this.setData({deptshow: false, profession: profession});
     // 从后端读该专业的教师数据
 
 
   },
   onTutorConfirm(event){
     const { picker, value, index } = event.detail;
-    var tutor = value[0];
+    var tutor = value;
     console.log(tutor);
-    this.setData({tutorshow: false});
+    this.setData({tutorshow: false, tutor: tutor});
     // 向后端写入教师学生绑定
 
 
   },
+  // 选学院时根据学院换专业分支
   onChange(event) {
     const { picker, value, index } = event.detail;
     picker.setColumnValues(1, dept[value[0]]);
