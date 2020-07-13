91957714dc8ab98d8fb8f24ac571abf57d4904ce
Yangfisher1 <Yangfisher1@sjtu.edu.cn>
2020-07-13 23:26:58 +0800

    bug: need to make python script correctly

--- crawler/DataManager.py
+++ crawler/DataManager.py
@@ -1,10 +1,13 @@
 import pymysql
 import threading
-from setting import MYSQL_DB, MYSQL_PWD, MYSQL_HOST, MYSQL_USER
+from setting import MYSQL_DB ,MYSQL_PWD, MYSQL_HOST, MYSQL_USER
+
+# 需要自己写sql脚本 自己指定需要连接的数据库
 
 
-class DataManager():
+class DataManager:
     # 单例模式，确保每次实例化都调用一个对象。
+    # 在这里出现的变量是一个类变量，它的值将在这个类的所有实例中共享
     _instance_lock = threading.Lock()
 
     def __new__(cls, *args, **kwargs):
@@ -22,13 +25,7 @@ class DataManager():
         # 建立游标
         self.cursor = self.conn.cursor()
 
-    def save_data(self, data):
-        # 数据库操作
-        # (1)定义一个格式化的sql语句
-        sql = 'insert into test(film_name) values(%s) '
-        # (2)准备数据
-        # data = ('nancy','30','100','太好笑了')
-        # (3)操作
+    def save_data(self, data, sql):
         try:
             self.cursor.execute(sql, data)
             self.conn.commit()
--- crawler/try2.py
+++ crawler/try2.py
@@ -1,12 +1,42 @@
+# coding=gbk
 import os
-path = 'data/selectPhase'
+import re
+
+
+def test(file):
+    pattern1 = re.compile(r'[\u4e00-\u9fa5]+')  # find student's name, teacher's name and status
+    pattern2 = re.compile(r'\d{5,13}')  # find the student's id and teacher's id
+    openfile = open(file, mode='r')
+    next(openfile)
+    lines = openfile.readlines()
+    i = 1
+    for line in lines:
+        if i % 2 == 0:
+            # 第二行 利用正则表达式提取学生ID 老师ID 老师姓名 当前的状态
+            result2 = pattern2.findall(line)    # sid and tid
+            print(result2[0])   # sid
+            if len(result2) != 1:   #
+                print(result2[1])   # tid
+            result3 = pattern1.findall(line)
+            result3.reverse()
+            print(result3[1])   # teacher's name
+            print(result3[0])   # status
+        else:
+            # 第一行 利用正则表达式提取 学生姓名
+            result1 = pattern1.findall(line)
+            print(result1[0])
+        i += 1
+    openfile.close()
+
+
+path = 'data/middlePhase'
 fileList = os.listdir(path)
 print(fileList)
 for item in fileList:
     filePath = path + '/' + item
-    file = open(filePath, mode='r')
-    next(file)
-    for line in file.readlines():
-        line = line.strip()
-        print(line)
-    file.close()
+    print(filePath)
+    test(filePath)
+
+
+
+
