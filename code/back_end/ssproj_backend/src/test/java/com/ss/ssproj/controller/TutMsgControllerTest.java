6dc3a58be710603864f474245e6baa71fdd70ad7
Gao-Ruize <499183353@qq.com>
2020-07-19 21:58:28 +0800

    feat: fininsh the tests for controller layer

--- code/back_end/ssproj_backend/src/test/java/com/ss/ssproj/controller/TutMsgControllerTest.java
+++ code/back_end/ssproj_backend/src/test/java/com/ss/ssproj/controller/TutMsgControllerTest.java
@@ -1,15 +1,17 @@
 package com.ss.ssproj.controller;
 
-import com.ss.ssproj.entity.Tutor;
+
+import com.alibaba.fastjson.JSONArray;
+import com.ss.ssproj.entity.*;
 import com.ss.ssproj.service.*;
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
-import org.junit.jupiter.api.BeforeAll;
+import org.json.JSONString;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -24,7 +26,9 @@ import org.springframework.web.context.WebApplicationContext;
 import javax.transaction.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -53,66 +57,178 @@ class TutMsgControllerTest {
     @MockBean
     private InstructService instructservice;
 
-//    @Autowired
-//    private TutorService tutorservice;
-//    @Autowired
-//    private InsMessageService insmessageservice;
-//    @Autowired
-//    private ReadInsMsgService readinsmsgservice;
-//    @Autowired
-//    private StudentService studentservice;
-//    @Autowired
-//    private InstructService instructservice;
-
-
-    List<Tutor> tut_list = new ArrayList<>();
-
     @BeforeEach
     public void setUp(){
         mockmvc = MockMvcBuilders.webAppContextSetup(webapplicatioincontext).build();
 
-        Tutor tut1 = new Tutor(1, "tut1", "uid1", "name1");
-        Tutor tut2 = new Tutor(2, "tut2", "uid2", "name2");
-        Tutor tut3 = new Tutor(3, "tut3", "uid3", "name3");
-        this.tut_list.clear();
-        this.tut_list.add(tut1);
-        this.tut_list.add(tut2);
-        this.tut_list.add(tut3);
+        Mockito.when(tutorservice.findAll()).thenAnswer(
+                new Answer<List<Tutor>>(){
+                    @Override
+                    public List<Tutor> answer(InvocationOnMock invocation){
+                        List<Tutor> tut_list = new ArrayList<>();
+                        tut_list.add(new Tutor(1, "tut1", "uid1", "name1"));
+                        tut_list.add(new Tutor(2, "tut2", "uid2", "name2"));
+                        tut_list.add(new Tutor(3, "tut3", "uid3", "name3"));
+                        return tut_list;
+                    }
+                }
+        );
+        Mockito.when(insmessageservice.saveOrUpdate(Mockito.any())).thenAnswer(
+                new Answer<InsMessage>(){
+                    @Override
+                    public InsMessage answer(InvocationOnMock invocation){
+                        return invocation.getArgument(0);
+                    }
+                }
+        );
+        Mockito.when(readinsmsgservice.save(Mockito.any())).thenAnswer(
+                new Answer<ReadInsMsg>(){
+                    @Override
+                    public ReadInsMsg answer(InvocationOnMock invocation){
+                        return invocation.getArgument(0);
+                    }
+                }
+        );
+        Mockito.when(readinsmsgservice.findAllByMsgid(Mockito.anyInt())).thenAnswer(
+                new Answer<List<ReadInsMsg>>(){
+                    @Override
+                    public List<ReadInsMsg> answer(InvocationOnMock invocation){
+                        List<ReadInsMsg> res = new ArrayList<>();
+                        ReadInsMsg msg1 = new ReadInsMsg(1, "sid1", 1, invocation.getArgument(0));
+                        ReadInsMsg msg2 = new ReadInsMsg(3, "sid2", 0, invocation.getArgument(0));
+                        res.add(msg1);
+                        res.add(msg2);
+                        return res;
+                    }
+                }
+        );
+        Mockito.when(studentservice.findDistinctByStudentId(Mockito.anyString())).thenAnswer(
+                new Answer<Student>(){
+                    @Override
+                    public Student answer(InvocationOnMock invocation){
+                        Student stu = new Student(1, invocation.getArgument(0), "uid", "name");
+                        return stu;
+                    }
+                }
+        );
+        Mockito.when(instructservice.findAllByTutorid(Mockito.anyString())).thenAnswer(
+                new Answer<List<Instruct>>(){
+                    @Override
+                    public List<Instruct> answer(InvocationOnMock invocation){
+                        List<Instruct> res = new ArrayList<>();
+                        Instruct ins1 = new Instruct(1, "sid1", invocation.getArgument(0));
+                        Instruct ins2 = new Instruct(2, "sid2", invocation.getArgument(0));
+                        res.add(ins1);
+                        res.add(ins2);
+                        return res;
+                    }
+                }
+        );
 
-        Mockito.when(tutorservice.findAll()).thenReturn(tut_list);
-
+        Mockito.when(insmessageservice.findAllByTutorid(Mockito.anyString())).thenAnswer(
+                new Answer<List<InsMessage>>(){
+                    @Override
+                    public List<InsMessage> answer(InvocationOnMock invocation){
+                        List<InsMessage> ret = new ArrayList<>();
+                        ret.add(new InsMessage(1, invocation.getArgument(0), "title1", "content1", "time1"));
+                        ret.add(new InsMessage(2, invocation.getArgument(0), "title2", "content2", "time2"));
+                        return ret;
+                    }
+                }
+        );
     }
 
     @Test
     public void tutorsTest() throws Exception {
-//        JSONArray array = new JSONArray();
-//        array.add(tut_list);
-//        String jsonString = array.toJSONString();
-        JSONArray obj = new JSONArray();
-        for(int i = 0; i < tut_list.size(); ++i){
-            obj.add(tut_list.get(i));
-        }
-        String exp = obj.toJSONString();
-        String result = mockmvc.perform(MockMvcRequestBuilders.get("/api/user/tutors"))
+        JSONArray arr = new JSONArray();
+        arr.add(new Tutor(1, "tut1", "uid1", "name1"));
+        arr.add(new Tutor(2, "tut2", "uid2", "name2"));
+        arr.add(new Tutor(3, "tut3", "uid3", "name3"));
+        JSONArray exp = JSONArray.parseArray(arr.toJSONString());
+        JSONArray res = JSONArray.parseArray(
+                mockmvc.perform(MockMvcRequestBuilders.get("/api/user/tutors"))
+                        .andExpect(status().isOk())
+                        .andReturn().getResponse().getContentAsString()
+        );
+
+        assertEquals(exp, res);
+    }
+
+    @Test
+    public void sendMsgTest() throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "tit1");
+        map.put("content", "content1");
+        map.put("time", "time1");
+        map.put("tutorId", "tutId1");
+        List<String> toIds = new ArrayList<>();
+        toIds.add("sId1");
+        toIds.add("sId2");
+        map.put("toIds", toIds);
+        String msgForm = JSONObject.toJSONString(map);
+        System.out.println(msgForm);
+        mockmvc.perform(MockMvcRequestBuilders.post("/api/tut/sendmsg")
+                .contentType(MediaType.APPLICATION_JSON).content(msgForm))
                 .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value(200));
 
-        //assertEquals(exp, result);
     }
 
     @Test
-    public void sendMsgTest() {
+    public void getMsgInfoTest() throws Exception {
+        int msgid = 2;
+        JSONArray res = JSONArray.parseArray(
+                mockmvc.perform(MockMvcRequestBuilders.get("/api/tut/getmsginfo/"+msgid))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString()
+        );
+
+        String sid1 = "sid1";
+        String sid2 = "sid2";
+        Student stu1 = new Student(1, sid1, "uid", "name");
+        stu1.setIfRead(1);
+        Student stu2 = new Student(1, sid2, "uid", "name");
+        stu2.setIfRead(0);
+        JSONArray arr = new JSONArray();
+        arr.add(stu1);
+        arr.add(stu2);
+        JSONArray exp = JSONArray.parseArray(arr.toJSONString());
+
+        assertEquals(exp, res);
     }
 
     @Test
-    public void getMsgInfoTest() {
+    public void getInsStudentsTest() throws Exception {
+        String tutid = "tut3";
+        JSONArray res = JSONArray.parseArray(
+                mockmvc.perform(MockMvcRequestBuilders.get("/api/tut/students/"+tutid))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString()
+        );
+
+        String sid1 = "sid1";
+        String sid2 = "sid2";
+        Student stu1 = new Student(1, sid1, "uid", "name");
+        Student stu2 = new Student(1, sid2, "uid", "name");
+        JSONArray arr = new JSONArray();
+        arr.add(stu1);
+        arr.add(stu2);
+        JSONArray exp = JSONArray.parseArray(arr.toJSONString());
+
+        assertEquals(exp, res);
     }
 
     @Test
-    public void getInsStudentsTest() {
-    }
+    public void getInsMagsTest() throws Exception {
+        String tutid = "tut4";
+        JSONArray res = JSONArray.parseArray(
+                mockmvc.perform(MockMvcRequestBuilders.get("/api/tut/insmsgs/"+tutid))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString()
+        );
 
-    @Test
-    public void getInsMagsTest() {
+        List<InsMessage> arr = new ArrayList<>();
+        arr.add(new InsMessage(1, tutid, "title1", "content1", "time1"));
+        arr.add(new InsMessage(2, tutid, "title2", "content2", "time2"));
     }
 }
