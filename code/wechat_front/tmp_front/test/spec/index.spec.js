036337867ade4e20b50487fba864a4617a1da95a
Gao-Ruize <499183353@qq.com>
2020-07-26 07:31:17 +0800

    feat: continue test for wechat project

--- code/wechat_front/tmp_front/test/spec/index.spec.js
+++ code/wechat_front/tmp_front/test/spec/index.spec.js
@@ -22,30 +22,84 @@ describe('index', () => {
     expect(await desc3.wxml()).toContain('确认');
 
     // 检测按钮
+    // await desc3.tap();
   });
 
-  it('messageDetail', async () => {
-    let page = await miniProgram.reLaunch('/pages/stuChooseTutor/stuChooseTutor');
-    // 先观察基本组件是否存在
-    let desc1 = await page.$('.page');
-    let desc2 = await page.$('.popup');
-    // console.log(await desc2.wxml())
-    expect(await desc1.wxml()).toContain('绑定导师');
-    expect(await desc2.wxml()).toContain('overlay');
-    // 检测按钮
-  }, 30000);
+
+  // it('messageDetail', async () => {
+  //   let page = await miniProgram.reLaunch('/pages/stuChooseTutor/stuChooseTutor');
+  //   // 先观察基本组件是否存在
+  //   let desc1 = await page.$('.page');
+  //   let desc2 = await page.$('.popup');
+  //   // console.log(await desc2.wxml())
+  //   expect(await desc1.wxml()).toContain('绑定导师');
+  //   expect(await desc2.wxml()).toContain('overlay');
+  //   // 检测按钮
+  // }, 30000);
+
 
   it('stuIndex', async () => {
     let page = await miniProgram.reLaunch('/pages/stuIndex/stuIndex');
     // 先观察基本组件是否存在
     let desc1 = await page.$('.title');
     let desc2 = await page.$('.msgContainer');
-    let desc3 = await page.$('.buttonContainer');
+    let showDetailButton = await page.$('.showDetail');
 
     expect(await desc1.wxml()).toContain(await page.data('phaseName'));
     expect(await desc2.wxml()).toContain('未读教务处信息');
-    expect(await desc3.wxml()).toContain('查看详情');
+    expect(await desc2.wxml()).toContain('未读导师信息');
+    expect(await showDetailButton.wxml()).toContain('查看详情');
     // 检测按钮
+    
+    await showDetailButton.tap();
+    await page.waitFor(3000);
+    const currentPage = await miniProgram.currentPage();
+    // console.log(desc3);
+    console.log(currentPage.path);
+    expect(currentPage.path).toContain('pages/stuMsgFromJwcPage/stuMsgFromJwcPage');
+    await miniProgram.navigateBack();
   }, 30000)
   
+
+  it('stuChooseTutor', async () => {
+    let page = await miniProgram.reLaunch('/pages/stuChooseTutor/stuChooseTutor');
+
+    let bindTutorText = await page.$('.bindTutor');
+    let selectedTutorText = await page.$('.selectedTutor');
+    let selectTutorButton = await page.$('.selectTutor');
+    let submitTutorButton = await page.$('.buttonClass');
+
+    expect(await bindTutorText.wxml()).toContain('绑定导师：');
+    expect(await selectedTutorText.wxml()).toContain('您的导师是：');
+    expect(await selectTutorButton.wxml()).toContain('选择导师');
+    expect(await submitTutorButton.wxml()).toContain('确认提交');
+
+    let mockTutors=[
+      {'tutorid': 1, 'tutorid': 'tutorid1', 'tutorname': 'tutorname1', 'uid': 'uid1'},
+      {'tutorid': 2, 'tutorid': 'tutorid2', 'tutorname': 'tutorname2', 'uid': 'uid2'},
+      {'tutorid': 3, 'tutorid': 'tutorid3', 'tutorname': 'tutorname3', 'uid': 'uid3'},
+    ];
+    await page.setData({
+      tutors: mockTutors,
+    });
+
+    console.log(await page.data());
+    expect(await page.data('showPop')).toEqual(false);
+    await selectTutorButton.tap();
+    expect(await page.data('showPop')).toEqual(true);
+
+    let tutorPicker = await page.$('.tutorpicker');
+console.log(tutorPicker);
+    let tutorOptions = await tutorPicker.$$('view');
+    console.log(tutorOptions);
+    //await tutorOptions[1].tap();
+
+  })
+
+  // it('stuMsgFromJwcPage', async () => {
+  //   let page = await miniProgram.reLaunch('/pages/stuMsgFromJwcPage/stuMsgFromJwcPage');
+
+
+  // })
+
 })
